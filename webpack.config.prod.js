2b81068eb3bc4b6a81c6d819625a63c3972c8ff7
Stephan Sum <sum@live.de>
2017-03-07 12:37:25 +0100

    fixed typo

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -78,7 +78,7 @@ module.exports = {
                     fallback: 'style-loader',
                     use: ['css-loader', 'postcss-loader']
                 })
-            ]},
+            },
             {
                 test: /\.(jpg|png|gif|ttf|eot|svg|woff|woff2)$/,
                 loader: 'url-loader',
